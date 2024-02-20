d92edf587970d4094105c4b5cfcac9be3ab5b80d
Arjun Gupta <140874081+arjungupta-15@users.noreply.github.com>
2024-02-20 23:00:44 +0530

    second

---
+++ Dom/index.js
@@ -0,0 +1,19 @@
+let head = document.getElementById("head");
+console.dir(head);
+
+let headings = document.getElementsByClassName("headings")
+console.log(headings);
+console.dir(headings);
+
+let param = document.getElementsByTagName("p")
+console.log(param);
+console.dir(param)
+
+let elements = document.querySelector("p");
+console.dir(elements);
+
+let allelements = document.querySelectorAll("p");
+console.dir(allelements);
+
+let nelements = document.querySelector(".headings");
+console.dir(nelements);
