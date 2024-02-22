6b74f678d5350a638281e1368d20314865dedc40
Arjun Gupta <140874081+arjungupta-15@users.noreply.github.com>
2024-02-22 23:02:38 +0530

    comm

--- Dom/index.js
+++ Dom/index.js
@@ -17,3 +17,10 @@ console.dir(allelements);
 
 let nelements = document.querySelector(".headings");
 console.dir(nelements);
+
+/*Qs. Create a H2 heading element with text - “Hello JavaScript”. Append “from Apna College students” to this text using JS.*/
+
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+
+h2.innerText = h2.innerText+"apna college";
---
+++ Events/index.js
@@ -0,0 +1,9 @@
+let but = document.querySelector("#but");
+but.onclick = ()=>{
+    console.log("my button is working")
+}
+
+let div = document.querySelector("div");
+div.onmouseover = ()=>{
+    console.log("tag was working")
+}
